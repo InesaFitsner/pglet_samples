0109d2b818d1f86f2ab41e305a8653048ac6412b
Inesa Fitsner <inesa@appveyor.com>
2021-01-22 11:53:51 -0800

    Update weight-converter.py

--- weight-converter.py
+++ weight-converter.py
@@ -8,23 +8,21 @@ page.clean()
 def on_click(e):
     
     product_index = products.index(page.get_value('product'))
-    from_value=int(page.get_value('from_value'))
-    from_unit=page.get_value('from_unit')
-    to_unit=page.get_value('to_unit')
+    from_value = int(page.get_value('from_value'))
+    from_unit_index = unit_names.index(page.get_value('from_unit'))
+    to_unit_index = unit_names.index(page.get_value('to_unit'))
 
     #set up conversion table depending on a product density
     density = densities[product_index]
     Tbsp = [1, 3, 0.5, 15*density, 15, 0.0625]
     tsp = [1/3, 1, 0.16, 5*density, 5, 0.0208333]
+    conversion_table = [Tbsp, tsp]
 
-    page.set_value('to_value', from_value*Tbsp[unit_names.index(to_unit)])
+    page.set_value('to_value', from_value*conversion_table[from_unit_index][to_unit_index])
 
 products = ['Flour', 'Butter', 'Sugar', 'Water']
 densities = [120/240, 227/240, 200/240, 240/240]
 unit_names = ['Tbsp', 'tsp', 'oz', 'g', 'ml', 'cup']
-x=1
-Tbsp = [1, 3, 0.5, 15*x, 15, 0.0625]
-tsp = [1/3, 1, 0.16, 5*x, 5, 0.0208333]
 
 
 
