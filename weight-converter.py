e16aa9b5b674a62350e6ddc72bc21971dae339a7
Inesa Fitsner <inesa@appveyor.com>
2021-01-22 11:07:16 -0800

    Update weight-converter.py

--- weight-converter.py
+++ weight-converter.py
@@ -6,11 +6,16 @@ page.update(Page(title="Weight Converter"))
 page.clean()
 
 def on_click(e):
-    page.add(Text(value='number'))
+    product_index = products.index(page.get_value('product'))
+    from_value=int(page.get_value('from_value'))
+    from_unit=page.get_value('from_unit')
+    to_unit=page.get_value('from_unit')
+    page.set_value('to_value', products[product_index])
 
-products = ['Flour', 'Butter', 'Sugar']
-density = [120/240, 227/240, 200/240]
-units = ['g', 'ml', 'oz', 'Tbsp', 'tsp', 'cup']
+products = ['Flour', 'Butter', 'Sugar', 'Water']
+density = [120/240, 227/240, 200/240, 240/240]
+unit_names = ['g', 'ml', 'oz', 'Tbsp', 'tsp', 'cup']
+
 
 
 page.add(Dropdown(id='product', label = 'Select product:', options = products))
@@ -19,13 +24,13 @@ page.add(
         Stack(horizontal = True, controls=[
             Text(value='Original quantity: '),
             Textbox(id='from_value', value = '0', align = 'right'),
-            Dropdown(id='from_unit', options = units),
+            Dropdown(id='from_unit', options = unit_names),
             Button(text='Go', onclick=on_click, data='Go')
             ]),
         Stack(horizontal = True, controls=[
             Text(value='Converted quantity: '),
             Textbox(id='to_value', value = '0', align = 'right'),
-            Dropdown(id='to_unit', options = units),
+            Dropdown(id='to_unit', options = unit_names),
             ]),
         )
                 
