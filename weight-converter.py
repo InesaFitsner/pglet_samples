86d8a6dfafc6d121511f6871083a9eb99c31c4f7
Inesa Fitsner <inesa@appveyor.com>
2021-01-28 09:49:54 -0800

    products are added to the pickle file

--- weight-converter.py
+++ weight-converter.py
@@ -1,22 +1,55 @@
-#import load_and_save
+#import create_products
+import pickle
 
 import pglet
 from pglet import Page, Text, Button, Stack, Textbox, Dropdown
 
 class Product():
     pass
+products = []
 
+def add_product(e):
+    '''
+    Reads in a new product and stores it
+    '''
+    print('Create new product')
+    #create a new instance
+    global products
+    new_product = Product()
+    #add the data attributes
+    #new_product.name = input('Enter the product name: ')
+    #new_product.density = input('Enter the product density: ')
+    new_product.name = page.get_value('product_name')
+    new_product.density = float(page.get_value('grams_in_a_cup'))/240
+    products.append(new_product)
+    print(products)
+    save_products('C:/Projects/Python/pglet_samples/products.txt')
 
+def save_products(file_name):
+    '''
+    saves the products as a pickled file
+    '''
+    print('Save contacts in ' +  file_name)
+    with open(file_name, 'wb') as out_file:
+        pickle.dump(products, out_file)
 
-page = pglet.page("index")
-page.update(Page(title="Weight Converter"))
-page.clean()
-#global products
+def load_products(file_name):
+    '''
+    Loads products from the pickled file
+    '''
+    global products
+    print('Load products from ' + file_name)
+    with open(file_name, 'rb') as input_file:
+        products = pickle.load(input_file)
 
-def on_click(e):
+#page = pglet.page("index")
+#page.update(Page(title="Weight Converter"))
+#page.clean()
+
+def convert(e):
     
     #number of a product in a list of product names
-    product_index = products.index(page.get_value('product'))
+    product_index = names.index(page.get_value('product'))
     try:
         from_value = float(page.get_value('from_value'))
         #if we get here the number is int
@@ -28,7 +61,7 @@ def on_click(e):
         #set up unit conversion values depending on a product density
         density = densities[product_index]
         
-        unit_in_ml = [15, 5, 29.5, 1/density, 1, 240]
+        unit_in_ml = [15, 5, 29.5, 1/float(density), 1, 240]
         page.set_value('to_value', from_value*unit_in_ml[from_unit_index]/unit_in_ml[to_unit_index])
         
         #printing product index to check
@@ -37,22 +70,52 @@ def on_click(e):
     except ValueError:
         page.send('set from_value errorMessage="Please enter a float number"') 
 
-products = ['Flour', 'Butter', 'Sugar', 'Water', 'Honey', 'Icing sugar', 'Brown sugar']
+#products = ['Flour', 'Butter', 'Sugar', 'Water', 'Honey', 'Icing sugar', 'Brown sugar']
 #products = load_and_save.load_list
-densities = [120/240, 227/240, 200/240, 240/240, 320/240, 125/240, 220/240]
+#densities = [120/240, 227/240, 200/240, 240/240, 320/240, 125/240, 220/240]
+
+#create initial products
+
+#add_product()
+#add_product()
+
+
+#save_products('C:/Projects/Python/pglet_samples/products.txt')
+
+load_products('C:/Projects/Python/pglet_samples/products.txt')
+
+names = []
+densities = []
+for product in products:
+    names.append(product.name)
+    densities.append(product.density)
+
+#print(names)
 
 units = ['Tbsp', 'tsp', 'oz', 'g', 'ml', 'cup']
 
+page = pglet.page("index")
+page.update(Page(title="Weight Converter"))
+page.clean()
 
+page.add(
+        Stack(horizontal = True, controls=[
+            Text(value='Product name: '),
+            Textbox(id='product_name', align = 'right'),
+            Text(value='Grams in a cup: '),
+            Textbox(id='grams_in_a_cup', align = 'right'),
+            Button(text='Add', onclick=add_product, data='Add')
+            ])
+        )
 
-page.add(Dropdown(id='product', label = 'Select product:', options = products, value = 'Water'))
+page.add(Dropdown(id='product', label = 'Select product:', options = names, value = 'Water'))
 
 page.add(
         Stack(horizontal = True, controls=[
             Text(value='Original quantity: '),
             Textbox(id='from_value', value = '0', align = 'right'),
             Dropdown(id='from_unit', options = units, value = 'g'),
-            Button(text='Go', onclick=on_click, data='Go')
+            Button(text='Go', onclick=convert, data='Go')
             ]),
         Stack(horizontal = True, controls=[
             Text(value='Converted quantity: '),
@@ -60,5 +123,7 @@ page.add(
             Dropdown(id='to_unit', options = units, value = 'g'),
             ]),
         )
+
+page.add(Text(value = names + densities))
 
 page.wait_close()
